ef87655b4abec84b0f6e4b369cc0a122acb9decd
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-07-16 23:57:18 +0900

    [FEAT] 성공/예외 처리 코드 업로드

---
+++ src/main/java/com/nexus/seoulmate/exception/status/ErrorStatus.java
@@ -0,0 +1,24 @@
+package com.nexus.seoulmate.exception.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorStatus {
+    // COMMON 4XX
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "COMMON400", "파라미터가 올바르지 않습니다."),
+    INVALID_BODY(HttpStatus.BAD_REQUEST, "COMMON400", "요청 본문이 올바르지 않습니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON404", "찾을 수 없는 리소스입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON404", "사용자를 찾을 수 없습니다"),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON405", "허용되지 않는 HTTP Method입니다.");
+
+    // 도메인별로
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/com/nexus/seoulmate/exception/status/SuccessStatus.java
@@ -0,0 +1,19 @@
+package com.nexus.seoulmate.exception.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus {
+    // SUCCESS 2XX
+    SUCCESS(HttpStatus.OK, "COMMON200", "요청이 성공적으로 처리되었습니다."),
+    CREATED(HttpStatus.CREATED, "COMMON201", "리소스가 성공적으로 생성되었습니다.");
+
+    // 도메인별로
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
