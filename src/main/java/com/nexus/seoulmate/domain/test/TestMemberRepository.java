b3cfdcda3ee82624dcd929a985fe5cad3784829c
hyowon <ally0204@naver.com>
2025-07-30 14:29:11 +0900

    필요없는 파일 삭제

--- src/main/java/com/nexus/seoulmate/domain/test/TestMemberRepository.java
+++
@@ -1,6 +0,0 @@
-//package com.nexus.seoulmate.domain.test;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface TestMemberRepository extends JpaRepository<TestMember, Long> {
-//}
