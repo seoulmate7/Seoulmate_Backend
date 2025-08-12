7fc923fd9a16fe6340b45c637f6193609c1cbce9
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-08-12 19:28:02 +0900

    [MOD/#46] 언어 map에 타입 String -> Languages로 변경

--- src/main/java/com/nexus/seoulmate/member/dto/signup/LevelTestRequest.java
+++ src/main/java/com/nexus/seoulmate/member/dto/signup/LevelTestRequest.java
@@ -1,5 +1,6 @@
 package com.nexus.seoulmate.member.dto.signup;
 
+import com.nexus.seoulmate.member.domain.enums.Languages;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,5 @@ import java.util.Map;
 @Schema(description = "언어 레벨 테스트 요청")
 public class LevelTestRequest {
     @Schema(description = "언어별 레벨", example = "{\"한국어\": 90, \"영어\": 82}")
-    private Map<String, Integer> languages = new HashMap<>();
+    private Map<Languages, Integer> languages = new HashMap<>();
 }
