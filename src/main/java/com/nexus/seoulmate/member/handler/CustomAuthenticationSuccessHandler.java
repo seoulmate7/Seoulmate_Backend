709d80e6e071b9456778da54ccdc4a15527fbdbc
Youngseo <135317722+youngseo22@users.noreply.github.com>
2025-08-21 18:16:56 +0900

    Update CustomAuthenticationSuccessHandler.java

--- src/main/java/com/nexus/seoulmate/member/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/nexus/seoulmate/member/handler/CustomAuthenticationSuccessHandler.java
@@ -60,7 +60,7 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
         }
         
         // 리디렉션 경로
-        response.sendRedirect("https://seoulmate-frontend.vercel.app/login/oauth2/code/google");
+        response.sendRedirect("http://localhost:5173/login/oauth2/code/google");
 
         // http://localhost:5173/login/oauth2/code/google
         // https://seoulmate-frontend.vercel.app/login/oauth2/code/google
